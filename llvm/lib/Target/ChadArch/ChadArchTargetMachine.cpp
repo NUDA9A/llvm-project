c9b94db7e59ec3bb4b488558c29f55244cbff9d1
Akimenko Ivan <vlakimenko2004@mail.ru>
2026-03-30 16:12:28 +0300

    [ChadArch] 2. Add Register Info for ChadArch architecture

--- llvm/lib/Target/ChadArch/ChadArchTargetMachine.cpp
+++ llvm/lib/Target/ChadArch/ChadArchTargetMachine.cpp
@@ -13,13 +13,14 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeChadArchTarget() {
 }
 
 ChadArchTargetMachine::ChadArchTargetMachine(const Target &T, const Triple &TT,
-                                   StringRef CPU, StringRef FS,
-                                   const TargetOptions &Options,
-                                   std::optional<Reloc::Model> RM,
-                                   std::optional<CodeModel::Model> CM,
-                                   CodeGenOptLevel OL, bool JIT)
+                                             StringRef CPU, StringRef FS,
+                                             const TargetOptions &Options,
+                                             std::optional<Reloc::Model> RM,
+                                             std::optional<CodeModel::Model> CM,
+                                             CodeGenOptLevel OL, bool JIT)
     : CodeGenTargetMachineImpl(
           T, "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32", TT, CPU, FS, Options,
           Reloc::Static, getEffectiveCodeModel(CM, CodeModel::Small), OL) {
   CHADARCH_DUMP_CYAN
+  initAsmInfo();
 }
