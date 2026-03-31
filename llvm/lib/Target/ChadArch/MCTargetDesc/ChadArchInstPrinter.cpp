207be7fca3a8e8f3e489742a4bbe83d99de6fc4b
Akimenko Ivan <vlakimenko2004@mail.ru>
2026-03-31 16:55:10 +0300

    [ChadArch] 17. Add full ChadArchInstPrinter

---
+++ llvm/lib/Target/ChadArch/MCTargetDesc/ChadArchInstPrinter.cpp
@@ -0,0 +1,46 @@
+#include "ChadArchInstPrinter.h"
+#include "MCTargetDesc/ChadArchInfo.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "asm-printer"
+
+#include "ChadArchGenAsmWriter.inc"
+
+void ChadArchInstPrinter::printRegName(raw_ostream &O, MCRegister Reg) {
+  O << getRegisterName(Reg);
+}
+
+void ChadArchInstPrinter::printInst(const MCInst *MI, uint64_t Address,
+                                    StringRef Annot, const MCSubtargetInfo &STI,
+                                    raw_ostream &O) {
+  printInstruction(MI, Address, O);
+  printAnnotation(O, Annot);
+}
+
+void ChadArchInstPrinter::printOperand(const MCInst *MI, int OpNo,
+                                       raw_ostream &O) {
+  const MCOperand &MO = MI->getOperand(OpNo);
+
+  if (MO.isReg()) {
+    printRegName(O, MO.getReg());
+    return;
+  }
+
+  if (MO.isImm()) {
+    O << MO.getImm();
+    return;
+  }
+
+  assert(MO.isExpr() && "Unknown operand kind in printOperand");
+  MO.getExpr()->print(O, &MAI);
+}
